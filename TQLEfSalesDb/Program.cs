389dbc3ba6828d0fa11d37eb94fd3054a68290d7
Greg Doud <gpdoud@gmail.com>
2021-08-06 13:39:01 -0400

    Committed by GitUtil

---
+++ TQLEfSalesDb/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+
+using TQLEfSalesDb.Models;
+
+namespace TQLEfSalesDb {
+
+    class Program {
+
+        static void Main(string[] args) {
+
+            var context = new SalesdbContext();
+
+            var customers = context.Customers.ToList();
+
+            var customer = context.Customers.Find(1);
+
+            var customersInCincinnati = context.Customers
+                                .Where(c => c.City == "Cincinnati")
+                                .ToList();
+
+            var newCustomer = new Customer() {
+                Name = "TQL", City = "Cincinnati", State = "OH",
+                Sales = 4000000, Active = true
+            };
+            //context.Customers.Add(newCustomer);
+            //var rowsAffected = context.SaveChanges();
+
+            //var tql = context.Customers.Find(43);
+            //tql.Name = "Total Quality Logistics";
+            //var rowsAffected = context.SaveChanges();
+
+            //tql = context.Customers.Find(43);
+            //context.Customers.Remove(tql);
+            //rowsAffected = context.SaveChanges();
+
+            var orders = context.Orders.ToList();
+
+
+        }
+    }
+}
